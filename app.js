03ab1696693d95592a5906c21def2cbe5b67ce60
Vladimir Pesovski <vladimirpesovski@Vladimirs-MacBook-Pro.local>
2019-10-28 16:11:19 -0400

    style: styling the footer

--- app.js
+++ app.js
@@ -1,39 +1,32 @@
 $(function() {
-
-        
-  
+   
 
         $("select").on("change", function() {
             let $selection = $(this).val()
-            // console.log($selection)
+            
             $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=jGnr0G8J3IgQZ3kmspywu5bYkvAD26Cr`)
             .done(function(data) {
-                $.each(data.results, function(key, value) {
-                    console.log(data.results)
-                $(".display").append(`<img src="${value.multimedia[4].url}"/>`)
-                $(".display").append(`<p>${value.title}</p>`)
-                console.log(value.title)
 
-                // $(".post").append(`<p>${value.icon}</p>`)
-                // $(".results").append(`<p>${value.id}</p>`)
-                // $(".results").append(`<p>${value.main}</p>`)
-                
-                
-                
+        
+            let counter = 0
 
+            $.each(data.results, function(key, value) {
+            console.log(value)
+            if (value.multimedia.length !== 0 && counter < 12) {
+            counter ++;
 
+            $(".display").append(`<img src="${value.multimedia[4].url}"/>`)
+            $(".display").append(`<p>${value.abstract}</p>`)
+
+            return counter !== 12
+
+            
+            }
+    
             });
         });
 
     });
-   
-
-
-
-
-
-
-
 
             
 })
