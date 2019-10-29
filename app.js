890d398b790f86fe1273290529c066ee4ffe1e01
Vladimir Pesovski <vladimirpesovski@Vladimirs-MacBook-Pro.local>
2019-10-29 15:45:35 -0400

    style: styling the links

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ $(function() {
    
 
         $("select").on("change", function() {
+            $('.display').empty();
             
             let $selection = $(this).val()
             $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=jGnr0G8J3IgQZ3kmspywu5bYkvAD26Cr`)
+            
             .done(function(data) {
+               
         
             let counter = 0
             $.each(data.results, function(key, value) {
@@ -14,8 +17,8 @@ $(function() {
             if (value.multimedia.length !== 0 && counter < 12) {
             counter ++;
             
-            $(".display").append(`<figure><img src="${value.multimedia[4].url}"/><p>${value.abstract}</p></figure>`)
-            // $(".display ").append(``)
+            $(".display").append(`<figure><a href ="${value.url}"target="_blank"><img src="${value.multimedia[4].url} "/><p>${value.abstract}</p></a></figure>`)
+            console.log(value);
        
            
             
