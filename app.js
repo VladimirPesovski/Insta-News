a715b8b431840e600d7c975329dcbffd73c9e410
Vladimir Pesovski <vladimirpesovski@Vladimirs-MacBook-Pro.local>
2019-10-29 11:45:27 -0400

    style: adding text on top of images

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
+
 $(function() {
    
 
         $("select").on("change", function() {
-            let $selection = $(this).val()
             
+            let $selection = $(this).val()
             $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$selection}.json?api-key=jGnr0G8J3IgQZ3kmspywu5bYkvAD26Cr`)
             .done(function(data) {
-
         
             let counter = 0
-
             $.each(data.results, function(key, value) {
-            console.log(value)
+            
             if (value.multimedia.length !== 0 && counter < 12) {
             counter ++;
-
-            $(".display").append(`<img src="${value.multimedia[4].url}"/>`)
-            $(".display").append(`<p>${value.abstract}</p>`)
-
+            
+            $(".display").append(`<figure><img src="${value.multimedia[4].url}"/><p>${value.abstract}</p></figure>`)
+            // $(".display ").append(``)
+       
+           
+            
             return counter !== 12
+
+
+           
+
+          
+
+
 
             
             }
@@ -31,6 +39,44 @@ $(function() {
             
 })
 
+// ======================
+
+// $(document).ready(function() {
+//     $(".loader").hide();
+//     $(".posts").on("change", function(event) {
+//       $("ul").empty();
+//       $(".loader").show();
+//       $change = $(this).val();
+//       $.getJSON(
+//         https://api.nytimes.com/svc/topstories/v2/${$change}.json?api-key=YTMJtmBtB00ippV8QvL3cjmtA9aRZvIf
+//       ).done(function(data) {
+//         let counter = 0;
+//         $.each(data.results, function(key, value) {
+//           console.log(value);
+//           let multimedia = value.multimedia;
+//           if (multimedia.length === 0) {
+//             return;
+//           }
+//           let description = value.abstract;
+//           let articleURL = value.url;
+//           $.each(multimedia, (key, value) => {
+//             if (value.format === "superJumbo") {
+//               let imageURL = value.url;
+//               let listItem = `<li><a href = "${articleURL}" target="_blank">
+//                       <img src = "${imageURL}"><p>${description}</p></a></li>`;
+//               $("ul").append(listItem);
+//               $("footer").css({"position":"relative", "padding-bottom": "30px"});
+//               $(".loader").hide();
+//             } else {
+//               return;
+//             }
+//           });
+//           counter++;
+//           return counter !== 12;
+//         });
+//       });
+//     });
+//    });
 
 
 
