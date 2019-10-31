6d49176930ed9aeceb1870e905af92516c8a6345
Vladimir Pesovski <vladimirpesovski@Vladimirs-MacBook-Pro.local>
2019-10-31 10:49:06 -0400

    style: styling the desktop footer

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ $(function() {
             
             .done(function(data) {
                
-        
             let counter = 0
             $.each(data.results, function(key, value) {
             
@@ -19,63 +18,11 @@ $(function() {
             
             $(".display").append(`<figure><a href ="${value.url}target="_blank"><img src="${value.multimedia[4].url} "/><p>${value.abstract}</p></a></figure>`)
             console.log(value);
-       
-           
-            
             return counter !== 12
-
-
-
-
-            
             }
     
             });
         });
-
-    });
-
-            
+    });           
 })
-
-// ======================
-
-// $(document).ready(function() {
-//     $(".loader").hide();
-//     $(".posts").on("change", function(event) {
-//       $("ul").empty();
-//       $(".loader").show();
-//       $change = $(this).val();
-//       $.getJSON(
-//         https://api.nytimes.com/svc/topstories/v2/${$change}.json?api-key=YTMJtmBtB00ippV8QvL3cjmtA9aRZvIf
-//       ).done(function(data) {
-//         let counter = 0;
-//         $.each(data.results, function(key, value) {
-//           console.log(value);
-//           let multimedia = value.multimedia;
-//           if (multimedia.length === 0) {
-//             return;
-//           }
-//           let description = value.abstract;
-//           let articleURL = value.url;
-//           $.each(multimedia, (key, value) => {
-//             if (value.format === "superJumbo") {
-//               let imageURL = value.url;
-//               let listItem = `<li><a href = "${articleURL}" target="_blank">
-//                       <img src = "${imageURL}"><p>${description}</p></a></li>`;
-//               $("ul").append(listItem);
-//               $("footer").css({"position":"relative", "padding-bottom": "30px"});
-//               $(".loader").hide();
-//             } else {
-//               return;
-//             }
-//           });
-//           counter++;
-//           return counter !== 12;
-//         });
-//       });
-//     });
-//    });
-
-
 
